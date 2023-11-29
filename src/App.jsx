bb27a2da0691cbf49efe135f53c37b5cfbe43fa5
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-28 21:53:44 -0500

    Complete 'FourOFour' page in 'App.jsx', 'FourOFour.jsx'.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // PAGES
+import FourOFour from './Pages/FourOFour';
 import Home from './Pages/Home';
 
 import './App.css';
@@ -13,6 +14,7 @@ function App() {
 				<main>
 					<Routes>
 						<Route path='/' element={<Home />} />
+						<Route path='*' element={<FourOFour />} />
 					</Routes>
 				</main>
 			</Router>
---
+++ src/Pages/FourOFour.jsx
@@ -0,0 +1,9 @@
+const FourOFour = () => {
+	return (
+		<div className='FourOFour'>
+			<h2>Sorry, no page found</h2>
+		</div>
+	);
+};
+
+export default FourOFour;
